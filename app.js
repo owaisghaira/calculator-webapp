b3f5bee8d81372e630cfd106cb0eb067c8887810
owaisghaira <owais.ghaira99@gmail.com>
2020-06-30 21:55:07 +0500

    final commit.

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-function setnum(){
+function setnum() {
     var name = document.getElementById('inputo')
     name.value = 0;
 }
 setnum()
 
-function getNumber(num){
+function getNumber(num) {
     var result = document.getElementById('inputo')
-    result.value +=num;
+    if (result.value === '0') {
+        result.value = "";
+        result.value += num;
 
+    }
+    else {
+        result.value += num;
+    }
 }
-function clearResult(){
+function clearResult() {
     var cl = document.getElementById('inputo')
     cl.value = " ";
 }
-function getResult(){
+function getResult() {
     var result = document.getElementById('inputo')
+    console.log(result.value)
     result.value = eval(result.value)
+}
+
+function clear1() {
+    var clear = document.getElementById('inputo')
+    clear.value = clear.value.slice(0, -1)
+
 }
